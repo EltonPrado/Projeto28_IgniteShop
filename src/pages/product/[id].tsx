11e4e239cb4b871357baec68e34719e7e6c27afe
elton <eltonm.prado@gmail.com>
2023-11-09 15:13:13 -0300

    Product & Checkout - Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,33 @@
 import { useRouter } from 'next/router'
+import { 
+  ImageContainer, 
+  ProductContainer, 
+  ProductDetails 
+} from '@/styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 89,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Veritatis, aliquid facilis sint delectus fuga consectetur 
+          perferendis, ea deserunt cum commodi dignissimos! Nisi, 
+          ad ducimus assumenda blanditiis iste voluptas sint dicta.
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
