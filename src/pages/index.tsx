4e5d4a4b849c22352eed18d54ced25356c192504
elton <eltonm.prado@gmail.com>
2023-11-09 11:57:56 -0300

    Product & Checkout - Navigation via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import { useKeenSlider } from 'keen-slider/react'
 
+import Link from 'next/link';
 import Image from "next/image";
 import Stripe from 'stripe';
 
@@ -30,14 +31,16 @@ export default function Home({ products }: HomeProps ) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-          <Product key={product.id} className='keen-slider__slide'>
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-        
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className='keen-slider__slide'>
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+          
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
