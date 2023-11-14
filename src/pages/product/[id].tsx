f9613bfbaa0363eb3d97cc47ea000c85a3dba075
elton <eltonm.prado@gmail.com>
2023-11-14 11:25:46 -0300

    Product & Checkout - Buy button

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 import { stripe } from '../../lib/stripe'
+import { useState } from 'react'
 
 import Stripe from 'stripe'
 import Image from 'next/image'
+import axios from 'axios'
 
 import { 
   ImageContainer, 
@@ -22,8 +24,23 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
-  function handleBuyProduct() {
-    console.log(product.defaultPriceId);
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+      alert('Falha ao redirecionar ao checkout')
+    }
   }
 
   return (
@@ -42,7 +59,10 @@ export default function Product({ product }: ProductProps) {
         <span>{product.price}</span>
         <p>{product.description}</p>
 
-        <button onClick={handleBuyProduct}>
+        <button 
+          disabled={isCreatingCheckoutSession}
+          onClick={handleBuyProduct}
+        >
           Comprar agora
         </button>
       </ProductDetails>
