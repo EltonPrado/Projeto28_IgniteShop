d5cdc3009717ebebd62e2eeada0e2de758c5e794
elton <eltonm.prado@gmail.com>
2023-11-14 14:57:30 -0300

    Product & Checkout - Success Framework

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,20 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <h1>Compra efetuada</h1>
+    <SuccessContainer>
+      <h1>Compra Efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul que empolgante! Sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa. 
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
